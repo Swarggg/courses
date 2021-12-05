81b7d7f1f2f3eb5201099dbaa45dfe29cc131bb6
Swarggg <swarg@inbox.ru>
2021-12-05 19:36:33 +0300

    адовое слияние массивов

---
+++ src/arrays.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class arrays {
+
+    public static void main(String[] args) {
+        String[] seasons = new String[4];
+
+        seasons[0] = "summer";
+        seasons[1] = "autumn";
+        seasons[2] = "winter";
+        seasons[3] = "spring";
+
+/*
+        String [] lodki = new String[] {"201", "202", "203", "204"};
+
+        String [] brothersNames = {"Sasha", "Vova"};
+
+        System.out.println(lodki[2].toString());
+        System.out.println(Arrays.toString(lodki));
+
+        for (int i=0; i<4; i++) {
+            System.out.println(seasons[i]);
+        }
+*/
+
+
+        int[] a1 = {2, 2, 4, 6};
+        int[] a2 = {1, 1, 3, 9};
+
+
+        int[] a3 = new int[a1.length + a2.length];
+
+        int i = 0, j = 0;
+        for (int k = 0; k < a3.length; k++) {
+
+        if (i > a1.length-1) { //проверка, не закончился ли массив а1
+            a3[k]= a2[j];
+            j++;
+        } else if (j> a2.length-1) { //проверка, не закончился ли массив а2
+            a3[k] = a1[i];
+            i++;
+        } else if (a1[i] < a2[j]) { //сравнение и запись в итоговый массив
+            a3[k] = a1[i];
+            i++;
+            } else {
+            a3[k] = a2[j];
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(a3));
+
+    }
+}
