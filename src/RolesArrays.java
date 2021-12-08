f1ca382521b3914ea5344f6f6747c60db59d503d
Swarggg <swarg@inbox.ru>
2021-12-09 01:28:08 +0300

    разбивка сценария

--- src/RolesArrays.java
+++ src/RolesArrays.java
@@ -1,13 +1,55 @@
+import java.util.Arrays;
+
 public class RolesArrays {
 
     public static void main(String[] args) {
 
-        //String emptyArguments = "";
+        String [] roles1= {
+                "Городничий","Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String [] textLines1={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Городничий Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
-       if (args[0].equals("h")) {
-           System.out.println("rguments = h");
-       }
+        System.out.println(printTextPerRole(roles1, textLines1));
+
+
+
     }
 
+    public static String printTextPerRole(String[] roles, String[] textLines) {
 
-}
+        int rowsQuantity = textLines.length;
+        int rolesQuantity = roles.length;
+
+        //System.out.println(rowsQuantity);
+
+        String[] screenplay = new String[rowsQuantity];
+
+        for (int j=0; j<rolesQuantity; j++) {
+            for (int i = 0; i < rowsQuantity; i++) {
+
+                //String detectRole = кусок строки из textLines до первого двоеточия;
+
+                if (textLines[i].contains(roles[j] + ":")) {
+                    screenplay[i] = Integer.toString(i + 1) + ")" + textLines[i];
+                }
+            }
+        }
+
+
+        //System.out.println(Arrays.toString(screenplay));
+        // screenplay[i] = roles[i];
+        return Arrays.toString(screenplay);
+        }
+
+
+
+
+
+    }
