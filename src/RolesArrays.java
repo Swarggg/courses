c5f13804dc8bc699a5714feb89efab9968acf9d3
Swarggg <swarg@inbox.ru>
2021-12-08 20:54:10 +0300

    адовое слияние массивов

---
+++ src/RolesArrays.java
@@ -0,0 +1,13 @@
+public class RolesArrays {
+
+    public static void main(String[] args) {
+
+        //String emptyArguments = "";
+
+       if (args[0].equals("h")) {
+           System.out.println("rguments = h");
+       }
+    }
+
+
+}
