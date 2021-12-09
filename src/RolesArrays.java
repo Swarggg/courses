e8f8b0391516647f182c2a9a04b71375f77e52a1
sma <swarg@inbox.ru>
2021-12-09 10:51:23 +0300

    почти готово

--- src/RolesArrays.java
+++ src/RolesArrays.java
@@ -11,14 +11,12 @@ public class RolesArrays {
         String [] textLines1={"Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
                 "Аммос Федорович: Как ревизор?",
                 "Артемий Филиппович: Как ревизор?",
-                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.", "Городничий: Господа, сообщаю вам пренеприятное известие, его поведал мне Артемий Филиппович: к нам едет ревизор.",
                 "Аммос Федорович: Городничий Вот те на!",
                 "Артемий Филиппович: Вот не было заботы, так подай!",
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
         System.out.println(printTextPerRole(roles1, textLines1));
-
-
 
     }
 
@@ -27,29 +25,23 @@ public class RolesArrays {
         int rowsQuantity = textLines.length;
         int rolesQuantity = roles.length;
 
-        //System.out.println(rowsQuantity);
-
-        String[] screenplay = new String[rowsQuantity];
+        String screenplay = new String ("");
 
         for (int j=0; j<rolesQuantity; j++) {
+
+            screenplay = screenplay+roles[j]+":\n";
+
             for (int i = 0; i < rowsQuantity; i++) {
 
-                //String detectRole = кусок строки из textLines до первого двоеточия;
-
                 if (textLines[i].contains(roles[j] + ":")) {
-                    screenplay[i] = Integer.toString(i + 1) + ")" + textLines[i];
+                    screenplay = screenplay+Integer.toString(i + 1) + ")" + textLines[i].replace(roles[j]+":","")+"\n";
+                        }
+                if (i==rowsQuantity-1) {
+                    screenplay=screenplay+"\n";
                 }
+
             }
         }
-
-
-        //System.out.println(Arrays.toString(screenplay));
-        // screenplay[i] = roles[i];
-        return Arrays.toString(screenplay);
-        }
-
-
-
-
-
+        return screenplay;
     }
+}
