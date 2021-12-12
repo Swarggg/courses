91058d32d78bfd824c0c273af6c52ba01ca7bebe
Swarggg <swarg@inbox.ru>
2021-12-12 18:57:03 +0300

    add all background for testing

---
+++ src/RobotMovement.java
@@ -0,0 +1,115 @@
+public class RobotMovement {
+
+    public static void main(String[] args) {
+
+
+        Robot robot = new Robot();
+        System.out.println("Start:\n"+"Y="+robot.getY()+"\nX="+robot.getX()+"\nDirect="+robot.nowDirection+"\n");
+
+        int targetX=2;
+        int targetY=3;
+
+        System.out.println("Target coordinate:\n"+"Y="+targetY+"\nX="+targetX+"\n");
+
+        moveRobot( robot,targetX, targetY);
+
+       System.out.println("Result:\n"+"Y="+robot.getY()+"\nX="+robot.getX()+"\nDirect="+robot.nowDirection);
+
+
+    }
+
+    public static class Robot  {
+
+        Direction nowDirection=Direction.UP;
+        int x = 0;
+        int y = 0;
+
+
+        public Direction getDirection() {
+            // текущее направление взгляда
+            return nowDirection;
+        }
+        public int getX() {
+            // текущая координата X
+            return x;
+        }
+        public int getY() {
+            // текущая координата Y
+            return y;
+        }
+        public void turnLeft() {
+            // повернуться на 90 градусов против часовой стрелки
+            switch (nowDirection) {
+                default:
+                case UP: nowDirection = Direction.LEFT;
+                    break;
+                case LEFT: nowDirection = Direction.DOWN;
+                    break;
+                case DOWN: nowDirection = Direction.RIGHT;
+                    break;
+                case RIGHT: nowDirection = Direction.UP;
+                    break;
+            }
+        }
+        public void turnRight() {
+            // повернуться на 90 градусов по часовой стрелке
+            switch (nowDirection) {
+                default:
+                case UP:nowDirection = Direction.RIGHT;
+                    break;
+                case LEFT:nowDirection = Direction.UP;
+                    break;
+                case DOWN:nowDirection = Direction.LEFT;
+                    break;
+                case RIGHT:nowDirection = Direction.DOWN;
+                    break;
+            }
+        }
+        public void stepForward() {
+            // шаг в направлении взгляда
+            // за один шаг робот изменяет одну свою координату на единицу
+            switch (nowDirection) {
+                case UP:y++;
+                    break;
+                case DOWN:y--;
+                    break;
+                case LEFT:x--;
+                    break;
+                case RIGHT:x++;
+                    break;
+            }
+        }
+        public enum Direction {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+    }
+
+
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+
+        robot.turnRight();
+        robot.stepForward();
+        robot.stepForward();
+
+        robot.turnLeft();
+        robot.stepForward();
+        robot.stepForward();
+        robot.stepForward();
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
