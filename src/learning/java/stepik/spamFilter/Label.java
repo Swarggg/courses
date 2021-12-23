44fbdc1ff01fa3e86ba865a2e3f23c9821595ebc
sma <swarg@inbox.ru>
2021-12-23 19:06:02 +0300

    begin spamFilter

---
+++ src/learning/java/stepik/spamFilter/Label.java
@@ -0,0 +1,5 @@
+package learning.java.stepik.spamFilter;
+
+public enum Label {
+    SPAM, NEGATIVE_TEXT, TOO_LONG, OK
+}
---
+++ src/learning/java/stepik/spamFilter/TextAnalyzer.java
@@ -0,0 +1,5 @@
+package learning.java.stepik.spamFilter;
+
+public interface TextAnalyzer {
+    Label processText(String text);
+}
