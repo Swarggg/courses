0dd8840f5b924186c1f4c6863869bfc4efd085eb
sma <swarg@inbox.ru>
2021-12-30 13:59:15 +0300

    continue Exception1

--- src/learning/java/stepik/exceptions/CallerMethod.java
+++ src/learning/java/stepik/exceptions/CallerMethod.java
@@ -2,20 +2,24 @@ package learning.java.stepik.exceptions;
 
 public class CallerMethod {
     public static void main(String[] args) {
-        System.out.println(getCallerClassAndMethodName());
+        System.out.println(" from main: "+getCallerClassAndMethodName());
         anotherMethod();
     }
 
     private static void anotherMethod() {
-        System.out.println(getCallerClassAndMethodName());
+        System.out.println(" from anozer: "+getCallerClassAndMethodName());
     }
 
     public static String getCallerClassAndMethodName() {
         String s= null;
-        if (s==null) throw new NullPointerException("!");
+        try {
+            if (s == null) throw new NullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println(e.getClass());
+           s=e.getMessage();
+        }
 
 
-
-        return null;
+        return s;
     }
 }
