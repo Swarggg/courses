523f42a3b4d2159350b7f787d48b29c63ac76897
Swarggg <swarg@inbox.ru>
2022-01-16 01:03:55 +0300

    begin chechsum

--- src/learning/java/stepik/streamLearn/DraftStream.java
+++ src/learning/java/stepik/streamLearn/DraftStream.java
@@ -11,7 +11,7 @@ public class DraftStream {
     public static void main(String[] args) throws IOException {
 
         copyFileContent(inputFile, outputFile);
-        copyFileContent(inputFile, outputFile);
+        //copyFileContent(inputFile, outputFile);
 
     }// main close
 
@@ -20,6 +20,20 @@ public class DraftStream {
              FileOutputStream fos = new FileOutputStream(outputFile)){
 
             byte[] byteBuffer1 = fis.readAllBytes();
+            byte[] byteBuffer2 = {0x33, 0x45, 0x01};
+            int rotatedNumber = Integer.rotateLeft(byteBuffer2[0], 1);
+            int xoredNumber = byteBuffer2[0]^rotatedNumber;
+
+            String rotatedNumberS = Integer.toBinaryString(rotatedNumber);
+            String number = Integer.toBinaryString(byteBuffer2[0]);
+            String xxoredNumberS = Integer.toBinaryString(xoredNumber);
+
+            System.out.println("namber:          "+number);
+            System.out.println("rotated namber: "+rotatedNumberS);
+            System.out.println("xored number:   "+xxoredNumberS);
+
+
+            //System.out.println("checkSum: "+ checkSum);
             //byte[] byteBuffer2 = { 0b00100000/*space, bin*/, 35/*#, dec*/, 040/*space, oct(start from 0)*/, 0x21/*!, hex*/};
 
             fos.write(byteBuffer1);
