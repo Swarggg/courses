f2dd6f7445f8265eba4236428b4126a65180f2a4
Swarggg <swarg@inbox.ru>
2022-01-23 02:30:58 +0300

    calc finished

--- src/learning/java/stepik/streamLearn/Main.java
+++ src/learning/java/stepik/streamLearn/Main.java
@@ -10,15 +10,15 @@ package learning.java.stepik.streamLearn;
 * */
 
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     static byte[] byteArr1 = {49, 32, 50, 32, 51, 10}; //1 2 3  Output 1: 6.000000
     static byte[] byteArr2 = {97, 49, 32, 98, 50, 32, 99, 51, 10}; //a1 b2 c3 Output 2: 0.000000
     static byte[] byteArr3 = {45, 49, 101, 51, 10, 49, 56, 32, 46, 49, 49, 49, 32, 49, 49, 98, 98, 98};
+    static String string1 = "1 2 3";
                                                                                 // -1e3
                                                                                 //18 .111 11bbb
                                                                                 // Output 3: -981.889000
@@ -26,17 +26,97 @@ public class Main {
     static  InputStream inputStream1 = new ByteArrayInputStream(byteArr1);
     static  InputStream inputStream2 = new ByteArrayInputStream(byteArr2);
     static  InputStream inputStream3 = new ByteArrayInputStream(byteArr3);
+    static  StringReader sr1 = new StringReader(string1);
+    //static DataInputStream dis = new DataInputStream(System.in);
+    //static Reader reader = new StringReader(dis.toString());
+
 
     public static void main(String[] args) throws IOException {
-        int b;
+        double dSum =0;
+        Scanner s = new Scanner(System.in);
 
-        System.out.print("Inputted stream: ");
-        while ( (b=inputStream1.read())!=-1) {
-           // System.out.print(b+" ");
+        while (s.hasNext()==true) {
+            try {
+               dSum += Double.parseDouble(s.next());
+                //System.out.println( dSum);
 
-            System.out.write(b);
+            } catch (NumberFormatException ime) {
+                
+            }
+
         }
+        String result= String.format("%.6f", dSum);
+        System.out.print(result);
 
+        /*
+        while (s.hasNextDouble()) {
+            
+
+            
+        }*/
+
+
+
+
+
+
+
+
+
+        /*       int readedFromSI;
+
+        try (InputStream inputStreamSI = System.in;
+             InputStreamReader isr = new InputStreamReader(inputStreamSI)) {
+
+            //в этот объект будет складываться всё, что введено в System.in
+            StringBuilder stringFromSI = new StringBuilder();
+
+            //читаем из System.in и собираем строку для разбора
+            while ((readedFromSI=isr.read())!=47) {
+                stringFromSI.append((char) readedFromSI);
+            }
+
+            System.out.print("stringFromSI="+stringFromSI.toString()+" ");
+            System.out.println("stringFromSI length="+stringFromSI.length());
+
+            //разбираем строку
+
+                try {
+                    double d = Double.parseDouble(stringFromSI.toString());
+                    System.out.println("d="+d);
+                } catch (NumberFormatException nfe) {
+
+                }
+
+            //System.out.println("d="+d);
+        }
+        //InputStreamReader isr = new InputStreamReader();
+
+        //System.out.println(dis.readDouble());
+        /*Reader reader = new StringReader(dis.readUTF());
+
+        Scanner scanner = new Scanner(reader);
+        d =scanner.nextDouble();
+        System.out.println (d );
+            //Scanner scanner = new Scanner(System.in);
+/*
+            try {double d = scanner.nextDouble();
+                System.out.println("d=" + d);}
+            catch (InputMismatchException ime) {
+                double d = scanner.nextDouble();
+                System.out.println("d=" + d);
+            }
+*/
+            }
+
+  /*
+        //System.out.print("Inputted stream: ");
+        while ( (b=inputStream3.read())!=-1) {
+          // System.out.print(b+" ");
+
+           // System.out.write(b);
+        }
+*/
     }
 
-}
+
