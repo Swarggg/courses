c34480e1119a0f1c22ef8efad99774c37f40a2e2
Swarggg <swarg@inbox.ru>
2022-01-23 22:50:17 +0300

    Robot interface, old

---
+++ src/learning/java/stepik/ConnectRobotToMove/RobotConnection.java
@@ -0,0 +1,8 @@
+package learning.java.stepik.ConnectRobotToMove;
+
+public interface RobotConnection extends AutoCloseable {
+    void moveRobotTo(int x, int y);
+
+    @Override
+    void close();
+}
