b694679b200cfe14f6f01c443aedc7d4bcca1f38
Swarggg <swarg@inbox.ru>
2022-01-26 00:29:17 +0300

    generics done

--- src/learning/java/stepik/generics/Pair.java
+++ src/learning/java/stepik/generics/Pair.java
@@ -1,9 +1,10 @@
 package learning.java.stepik.generics;
 
 import java.util.Objects;
+import java.util.Optional;
 
 
-public class Pair <T1, T2> {
+public class Pair<T1, T2> {
 
     private final T1 value1;
     private final T2 value2;
@@ -15,18 +16,19 @@ public class Pair <T1, T2> {
         this.value2 = value2;
     }
 
-    public <T1, T2> Pair<T1, T2> of(T1 value1, T2 value2) {
 
-        return new Pair<>(value1, value2);
+
+    public static  <T1, T2> Pair<T1, T2> of(T1 value1, T2 value2) {
+
+        return new Pair<> (value1, value2);
     }
 
 
-
-    public T1 getValue1() {
+    public T1 getFirst() {
         return value1;
     }
 
-    public T2 getValue2() {
+    public T2 getSecond() {
         return value2;
     }
 
@@ -36,13 +38,14 @@ public class Pair <T1, T2> {
         if (o == null || getClass() != o.getClass()) return false;
         Pair<?, ?> pair = (Pair<?, ?>) o;
         return Objects.equals(value1, pair.value1) && Objects.equals(value2, pair.value2);
+        // f1.equals(f2) - неправильно, получите NullPointerException при определенных значениях
+        //Objects.equals(f1, f2) && Objects.equals(s1,s2) - корректный вариант сравнения first и second
+
     }
 
     @Override
     public int hashCode() {
+
         return Objects.hash(value1, value2);
     }
-
-
-
 }
