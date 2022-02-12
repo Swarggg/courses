4b404b78201f9b6d834c2e4b4550369346ef2a77
Swarggg <swarg@inbox.ru>
2022-02-12 13:38:11 +0300

    trining some lambda & function interface

--- src/learning/java/stepik/collectionDraft/MainCollect.java
+++ src/learning/java/stepik/collectionDraft/MainCollect.java
@@ -1,18 +1,24 @@
 package learning.java.stepik.collectionDraft;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class MainCollect {
 
+    //data
+    static List<User> users = Arrays.asList(
+            new User(21, "Vasya", Role.USER),
+            new User(1, "Admin", Role.ADMIN),
+            new User(123, "Pupik", Role.GUEST),
+            new User(234, "Lupik", Role.GUEST),
+            new User(2342, "Dupik", Role.GUEST),
+            new User(2342, "Mupik", Role.GUEST)
+    );
+
     public  void testOldCollection_Java7() {
-        //data
-        Collection<User> users = Arrays.asList(
-                new User(21, "Vasya", Role.USER),
-                new User(1, "Admin", Role.ADMIN),
-                new User(123, "Pupik", Role.GUEST),
-                new User(234, "Lupik", Role.GUEST),
-                new User(2342, "Dupik", Role.GUEST)
-        );
+
 
         System.out.println("Guests: "+users.size());
 
@@ -36,20 +42,59 @@ public class MainCollect {
             }
         });
 
-
-       /* //map - применить ко всем
-        List <String> names = new LinkedList<>();
-        for (User user: guestsFromUsers) {
-            names.add(user.getName());
-        }
-*/
-
     } //testOldCollection closing
 
-    public static void main(String[] args) {
 
-        MainCollect mc = new MainCollect();
-        mc.testOldCollection_Java7();
+    public void testCollection_Java8 () {
+
+        List<User> usr = Arrays.asList(
+                new User(21, "Vasya", Role.USER),
+                new User(1, "Admin", Role.ADMIN),
+                new User(123, "Pupik", Role.GUEST),
+                new User(234, "Lupik", Role.GUEST),
+                new User(2342, "Dupik", Role.GUEST)
+        );
+
+
+        User dude= usr.get(0);
+        System.out.println("name 1 ="+dude.getName());
+
+
+
+        List<User> namesOfGuests = usr.stream()
+                .filter(user -> user.getRole()==Role.GUEST)
+               // .sorted((obj1, obj2) -> Integer.compare(obj1.getId(), obj2.getId()))
+               // .map(user -> user.getName())
+                .collect(Collectors.toList());
+
+        int size = namesOfGuests.size();
+        System.out.println("size="+size);
+
+    }
+
+
+    public static void main(String[] args) {
+/*
+         List<User> users1 = new ArrayList<>();
+                users1.add(new User(21, "Vasya", Role.USER));
+
+                new User(1, "Admin", Role.ADMIN),
+                new User(123, "Pupik", Role.GUEST),
+                new User(234, "Lupik", Role.GUEST),
+                new User(2342, "Dupik", Role.GUEST),
+                new User(2342, "Mupik", Role.GUEST)
+
+
+        System.out.println(users1.size());
+        User u1 = (User) users1.get(0);
+
+        String k =(String) u1.getName();
+
+
+        System.out.println("k="+k);
+
+
+             */
 
     }
 
---
+++ src/learning/java/stepik/collectionDraft/WhyNot.java
@@ -0,0 +1,54 @@
+package learning.java.stepik.collectionDraft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WhyNot {
+
+    public static class UserIn {
+        private int id;
+        private String name;
+        private Role role;
+
+        UserIn (int id, String name, Role role) {
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getName () {
+            return this.name;
+        }
+
+        public Role getRole () {
+            return this.role;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        //listA
+        List<UserIn> listA = new ArrayList<>();
+        listA.add(new UserIn(23,"Admin",Role.ADMIN));
+
+        UserIn element1 =  listA.get(0);
+
+        System.out.println("el1 = "+ element1.getName());
+
+
+//listB
+        List<String> listB = new ArrayList<>();
+        listB.add("fooooorik");
+
+        String firstEl= listB.get(0);
+
+        int length = listB.get(0).length();
+        System.out.println("firstEl = "+firstEl+" and length = "+length);
+
+    }
+
+
+}
